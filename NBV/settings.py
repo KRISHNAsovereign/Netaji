1256043ea60266197880fcf9a75442a7f963d38e
KRISHNAsovereign <101880819+KRISHNAsovereign@users.noreply.github.com>
2023-08-29 12:06:11 +0530

    virtual

--- NBV/settings.py
+++ NBV/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1' ]
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- NBV/wsgi.py
+++ NBV/wsgi.py
@@ -7,13 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 import os
+from whitenoise import WhiteNoise
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NBV.settings')
-
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
+
+# Add any additional directories of static files
+application.add_files(os.path.join(os.path.dirname(__file__), 'static'), prefix='static/')
+
+# Set the WSGI application
 app = application
